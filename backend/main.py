088afd1978f16ccdb2f3535bfb7408c315baa37b
Pratham Mohan <pratham.parker14@gmail.com>
2025-07-02 22:28:19 +0530

    removed static directory

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 
 templates = Jinja2Templates(directory="backend/jinja2templates")
-app.mount("/static", StaticFiles(directory="backend/static"), name="static")
+
 
 
 @app.get("/")
